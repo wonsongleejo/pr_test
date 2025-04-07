e3b16963dac7ddeb40927832452164aaa4ddb594
cho <websera12@gmail.com>
2025-04-07 14:33:11 +0900

    init commit

---
+++ src/main/java/com/monkey/pr_test/PrTestApplication.java
@@ -0,0 +1,13 @@
+package com.monkey.pr_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrTestApplication.class, args);
+    }
+
+}
